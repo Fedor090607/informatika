0781cb9550ba60c88350fab324a6f5db5a951bb1
Fedor090607 <123183014+Fedor090607@users.noreply.github.com>
2025-10-02 00:34:36 +0300

    Add files via upload

---
+++ практика4_вариант15.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+int main() {
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    float x, y;
+    std::cout << "Введите X и Y: ";
+    std::cin >> x >> y;
+  
+    float R = asinf(y / x);
+    float S = exp(-3 * x) + cbrt(y);
+
+    std::cout << "R = " << R << std::endl;
+    std::cout << "S = " << S << std::endl;
+
+    float C = std::fmax(R, S);
+
+    std::cout << "Максимальное значение C = " << C << std::endl;
+    system("pause");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
