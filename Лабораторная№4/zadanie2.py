10fdb0f8ba44f4d6574447416733c9527442bc9f
Fedor090607 <123183014+Fedor090607@users.noreply.github.com>
2025-12-11 21:43:47 +0300

    Update zadanie2.py

--- Лабораторная№4/zadanie2.py
+++ Лабораторная№4/zadanie2.py
@@ -1,60 +1,50 @@
-def TolkoIzA_MensheT(S1, S2, t):
-    Result = []
-    for a in S1:
-        if a not in S2 and a < t:
-            Result.append(a)
-    return Result
-S1 = []
-S2 = []
+def TolkoIzAMensheT(S1, S2, t):
+    return sorted({x for x in S1 - S2 if x < t})
 while True:
     try:
-        Size1 = int(input("\nсколько чисел будет в множестве A: "))
+        Size1 = int(input("cколько чисел будет в множестве A: "))
         break
     except ValueError:
         print("ошибка, введите ЦЕЛОЕ ЧИСЛО")
+
 print(f"введите {Size1} чисел для множества A:")
-for i in range(Size1):
-    while True:
-        try:
-            N = int(input())
-            S1.append(N)
-            break
-        except ValueError:
-            print("ошибка, введите целые ЧИСЛА")
+S1 = set()
+while len(S1) < Size1:
+    try:
+        N = int(input())
+        S1.add(N)
+    except ValueError:
+        print("ошибка, введите целые ЧИСЛА")
 
 while True:
     try:
-        Size2 = int(input("\nсколько чисел будет в множестве B: "))
+        Size2 = int(input("cколько чисел будет в множестве B: "))
         break
     except ValueError:
         print("ошибка, введите ЦЕЛОЕ ЧИСЛО")
+
 print(f"введите {Size2} чисел для множества B:")
-for i in range(Size2):
-    while True:
-        try:
-            N = int(input())
-            S2.append(N)
-            break
-        except ValueError:
-            print("ошибка, введите целые ЧИСЛА")
+S2 = set()
+while len(S2) < Size2:
+    try:
+        N = int(input())
+        S2.add(N)
+    except ValueError:
+        print("ошибка, введите целые ЧИСЛА")
 
 while True:
     try:
-        t = int(input("\nвведите значение порога t: "))
+        t = int(input("введите значение порога t: "))
         break
     except ValueError:
         print("ошибка, введите ЦЕЛОЕ ЧИСЛО")
+Itogi = TolkoIzAMensheT(S1, S2, t)
 
-Itogi = TolkoIzA_MensheT(S1, S2, t)
-
-print("\nрезультат")
-if len(Itogi) == 0:
+print("результат:")
+if not Itogi:
     print("нет элементов, которые только в A и меньше t")
 else:
     print(f"элементы только из A и меньше {t}: {Itogi}")
-
-
-
 
 
 
