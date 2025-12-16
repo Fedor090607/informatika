42c2d46dfcf6d389079707a6ff2c73bfa9914bfc
Fedor090607 <123183014+Fedor090607@users.noreply.github.com>
2025-12-16 22:51:34 +0300

    Update mean.cpp

--- Лабораторная№7/mean.cpp
+++ Лабораторная№7/mean.cpp
@@ -1,1 +1,23 @@
+#include "pch.h"
+#define STDMODULE_EXPORTS
+#include "mean.h"  // Заголовочный файл для функции среднего
+#include <chrono>
+
+STDMODULE_API float calculate_mean(float* array, size_t size, double* elapsed_time_ms) {
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    float sum = 0.0f;
+    for (size_t i = 0; i < size; ++i) {
+        sum += array[i];
+    }
+    float mean = sum / static_cast<float>(size);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration_ms = end - start;
+
+    *elapsed_time_ms = duration_ms.count();
+
+    return mean;
+}
 
