7350e6efe860c03e83fb822afefc2cabeb313c24
Fedor090607 <123183014+Fedor090607@users.noreply.github.com>
2025-12-16 22:53:43 +0300

    Update mean.h

--- Лабораторная№7/mean.h
+++ Лабораторная№7/mean.h
@@ -1,1 +1,16 @@
+#pragma once
+#ifdef _WIN32
+#ifdef STDMODULE_EXPORTS
+#define STDMODULE_API __declspec(dllexport)
+#else
+#define STDMODULE_API __declspec(dllimport)
+#endif
+#else
+#define STDMODULE_API __attribute__((visibility("default")))
+#endif
+#include <cstddef>
+
+extern "C" {
+    STDMODULE_API float calculate_mean(float* array, size_t size, double* elapsed_time_ms);
+}
 
